28af88809ed61efbc18f5cdf180408f212931492
Natalie Misasi <87094477+nataliemisasi@users.noreply.github.com>
2021-09-17 17:49:28 -0500

    Update watermark_image.py
    updated initial add_watermark to indicate text
    added watermark_with_transparency

--- watermarking_images/watermark_image.py
+++ watermarking_images/watermark_image.py
@@ -1,15 +1,35 @@
 from PIL import Image, ImageFont, ImageDraw
-def add_watermark_text(original_input_path, save_image_path, text, pos):
-    photo = Image.open(original_input_path)
+
+def add_watermark_text(original_image_path, save_image_path, text, pos):
+    photo = Image.open(original_image_path)
 
     drawing = ImageDraw.Draw(photo)
 
-    black = (3, 8, 12)
-    font = ImageFont.truetype("Satisfy-Regular.ttf", 40)
-    drawing.text(pos, text, fill=black, font=font)
+    color = (80, 79, 84)
+    font = ImageFont.truetype("fonts/Rhesmanisa.otf", 70)
+    drawing.text(pos, text, fill=color, font=font)
     photo.show()
     photo.save(save_image_path)
 
 if __name__ == '__main__':
-    img = 'tour_da_parish.jpg'
-    add_watermark_text(img, 'tour_da_parish_watermarked.jpg', text='Natalie Misasi', pos=(5, 5))
+    img = 'photos/tour_da_parish.jpg'
+    add_watermark_text(img, 'photos/watermarked/tour_da_parish_text_watermark.jpg', text='Natalie Misasi', pos=(15, 35))
+
+# ___________________________________________________________________________________________________________
+
+def watermark_with_transparency(original_image_path, save_image_path, watermark_image_path, position):
+
+    base_image = Image.open(original_image_path)
+    watermark = Image.open(watermark_image_path).convert("RGBA")
+    width, height = base_image.size
+
+    transparent = Image.new('RGBA', (width, height), (255, 255, 255, 0))
+    transparent.paste(base_image, (0, 0))
+    transparent.paste(watermark, position, mask=watermark)
+    transparent.show()
+    transparent.save(save_image_path)
+
+
+if __name__ == '__main__':
+    img = 'photos/tour_da_parish.jpg'
+    watermark_with_transparency(img, 'photos/watermarked/tour_da_parish_pic_watermark.jpg', 'transparent_signature_watermark.png',  position=(15, 590))
